9635fea46153a68e922de2722f9131152eacc1d2
qpritchett <95952743+qpritchett@users.noreply.github.com>
2023-08-27 17:35:32 -0600

    Added SplitExpression and setup tests

--- Spreadsheet/FormulaEvaluator/Evaluator.cs
+++ Spreadsheet/FormulaEvaluator/Evaluator.cs
@@ -1,7 +1,36 @@
-﻿namespace FormulaEvaluator;
+﻿using System.Text.RegularExpressions;
 
-public static class Evaluator
-{
-    // TODO: Follow the PS1 instructions
+namespace FormulaEvaluator;
+
+public static class Evaluator {
+    public delegate int Lookup(String v);
+
+    public static int Evaluate(String exp) {
+
+        Stack<int> values = new Stack<int>();
+        Stack<string> operators = new Stack<string>();
+
+        string operatorSrings = "+-*-()";
+
+        string[] tokens = SplitExpression(exp);
+
+        foreach(string token in tokens) {
+            if (String.IsNullOrEmpty(token)) {
+                continue;
+            }
+        }
+
+        return 0;
+    }
+
+    public static string[] SplitExpression(string exp) {
+
+        string[] substrings = Regex.Split(exp, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+
+        for (int i = 0; i < substrings.Length; i++) {
+            substrings[i] = substrings[i].Trim();
+        }
+        return substrings;
+    }
+    
 }
-
--- Spreadsheet/FormulaEvaluatorTester/Program.cs
+++ Spreadsheet/FormulaEvaluatorTester/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string expression = "( 2 + 35 ) * A7 + B88 * 4";
+string[] splitExpression = FormulaEvaluator.Evaluator.SplitExpression(expression);
+foreach (string item in splitExpression) {
+    Console.WriteLine(item);
+}
 
+FormulaEvaluator.Evaluator.Evaluate(expression);
+
+
+Console.Read();
