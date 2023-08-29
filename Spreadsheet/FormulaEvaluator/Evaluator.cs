732e661622d1ef774b572f254d24802992ca4e18
qpritchett <95952743+qpritchett@users.noreply.github.com>
2023-08-28 21:22:49 -0600

    Initial logic and basic tests

--- Spreadsheet/FormulaEvaluator/Evaluator.cs
+++ Spreadsheet/FormulaEvaluator/Evaluator.cs
@@ -1,26 +1,121 @@
-﻿using System.Text.RegularExpressions;
+﻿///
+/// Evaluator.cs
+/// FormulaEvaluator
+///
+/// Created 8/27/23
+///
+
+using System.Text.RegularExpressions;
 
 namespace FormulaEvaluator;
 
 public static class Evaluator {
     public delegate int Lookup(String v);
 
-    public static int Evaluate(String exp) {
+    public static int Evaluate(String exp) { //,Lookup variableEvaluator
 
         Stack<int> values = new Stack<int>();
         Stack<string> operators = new Stack<string>();
 
-        string operatorSrings = "+-*-()";
-
         string[] tokens = SplitExpression(exp);
 
-        foreach(string token in tokens) {
+        foreach (string token in tokens) {
             if (String.IsNullOrEmpty(token)) {
                 continue;
             }
+
+            // Integer case 
+            if (int.TryParse(token, out int intToken)) {
+                if (values.Count > 0) {
+                    string topOperator = operators.Peek();
+
+                    if (topOperator == "*" || topOperator == "/") {
+                        int topValue = values.Pop();
+                        topOperator = operators.Pop();
+
+                        int result = Operate(topValue, topOperator, intToken);
+                        values.Push(result);
+                    } else { values.Push(intToken); }
+                }
+                else { values.Push(intToken); }
+            }
+
+            // Variable case
+
+            // + or - case
+
+            else if (token == "+" || token == "-") {
+                if (operators.Count > 0) {
+                    string topOperator = operators.Peek();
+                    if (topOperator == "+" || topOperator == "-") {
+                        int value1 = values.Pop();
+                        int value2 = values.Pop();
+
+                        topOperator = operators.Pop();
+
+                        int result = Operate(value1, topOperator, value2);
+                        values.Push(result);
+
+                        operators.Push(token);
+                    }
+                }
+                operators.Push(token);
+            }
+
+            // * or / case
+            else if (token == "*" || token == "/") {
+                operators.Push(token);
+            }
+
+            else if (token == "(") {
+                operators.Push(token);
+            }
+            else if (token == ")") {
+                string topOperator = operators.Peek();
+                if (topOperator == "+" || topOperator == "-") {
+                    if (values.Count < 2) {
+
+                    }
+
+                    int value1 = values.Pop();
+                    int value2 = values.Pop();
+                    topOperator = operators.Pop();
+
+                    int result = Operate(value1, topOperator, value2);
+
+                    values.Push(result);
+
+                    if (operators.Peek() == "("){
+                        operators.Pop();
+                    }
+                    else {
+                        throw new ArgumentException("Bad expression");
+                    }
+                }
+                else if (topOperator == "*" || topOperator == "/") {
+
+                    int value1 = values.Pop();
+                    int value2 = values.Pop();
+                    topOperator = operators.Pop();
+
+                    int result = Operate(value1, topOperator, value2);
+
+                    values.Push(result);
+                }
+            }      
         }
 
-        return 0;
+        if (operators.Count == 0) {
+            return values.Pop();
+        }
+        else if (operators.Count == 1 && values.Count == 2) {
+            int value1 = values.Pop();
+            int value2 = values.Pop();
+            string topOperator = operators.Pop();
+
+            return Operate(value1, topOperator, value2);
+        }
+        else throw new Exception("Failed to evaluate expression");
     }
 
     public static string[] SplitExpression(string exp) {
@@ -31,6 +126,26 @@ public static class Evaluator {
             substrings[i] = substrings[i].Trim();
         }
         return substrings;
+    }
+
+    static int Operate(int value1, string op, int value2) {
+
+
+        switch (op) {
+            case "+":
+                return value1 + value2;
+            case "-":
+                return value1 - value2;
+            case "*":
+                return value1 * value2;
+            case "/":
+                if (value2 == 0) {
+                    throw new ArgumentException("Illegal: division by zero");
+                }
+                return value1 / value2; //Integer division 
+            default:
+                throw new ArgumentException("Illegal operator");
+        }
     }
     
 }
--- Spreadsheet/FormulaEvaluatorTester/Program.cs
+++ Spreadsheet/FormulaEvaluatorTester/Program.cs
@@ -1,12 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-string expression = "( 2 + 35 ) * A7 + B88 * 4";
+﻿///
+/// A test program for the Evaluator class
+///
+/// 
+///
+string expression = "( 2 + 35 )"; // 37
+string expression2 = "(2 + 3) / 2 + 2"; // 4
+string expression3 = " 2/ 0"; // Error: Divide by Zero
+string expression4 = ""; // Error: Empty/Bad Argument
+string expression5 = "((2+2)+2)"; //6
+
+//Test the expression splitter
 string[] splitExpression = FormulaEvaluator.Evaluator.SplitExpression(expression);
 foreach (string item in splitExpression) {
-    Console.WriteLine(item);
+    //Console.WriteLine(item);
 }
 
-FormulaEvaluator.Evaluator.Evaluate(expression);
-
+int result = FormulaEvaluator.Evaluator.Evaluate(expression5);
+Console.WriteLine(result);
 
 Console.Read();
