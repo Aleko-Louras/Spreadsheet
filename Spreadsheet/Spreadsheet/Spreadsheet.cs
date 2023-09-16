b8227ea7f064e20d2866c4fa1922a9331bed7c2c
qpritchett <95952743+qpritchett@users.noreply.github.com>
2023-09-15 21:00:30 -0600

    Begin PS4

---
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Spreadsheet {
+    public class Spreadsheet {
+        public Spreadsheet() {
+
+        }
+    }
+}
+
---
+++ Spreadsheet/SpreadsheetTests/SpreadsheetUnitTests.cs
@@ -0,0 +1,9 @@
+﻿namespace SpreadsheetTests;
+
+[TestClass]
+public class SpreadsheetUnitTests {
+    [TestMethod]
+    public void TestMethod1() {
+
+    }
+}
