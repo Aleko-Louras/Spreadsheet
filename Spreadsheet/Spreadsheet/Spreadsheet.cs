c9d1b7ecc0034e38244efd48462bfad954c0ab5e
qpritchett <95952743+qpritchett@users.noreply.github.com>
2023-09-18 21:56:35 -0600

    Updated IsValid and Cell class
    Empty cells now properly return empty strings.
    
    Valid Cell names are recognized as valid.

--- Spreadsheet/Spreadsheet/Spreadsheet.cs
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -10,10 +10,22 @@ using System.Text.RegularExpressions;
 using SpreadsheetUtilities;
 
 namespace SS {
+
+    /// <summary>
+    /// An implementation of the AbstractSpreadsheet
+    /// </summary>
     public class Spreadsheet : AbstractSpreadsheet {
 
-        private Dictionary<string, Cell> Cells = new();
-        private DependencyGraph DG = new();
+        /// <summary>
+        /// A mapping from names to Cell objects 
+        /// </summary>
+        private readonly Dictionary<string, Cell> NonEmptyCells = new();
+
+        /// <summary>
+        /// A dependency graph for keeping track of dependencies
+        /// within the spreadsheet
+        /// </summary>
+        private readonly DependencyGraph DG = new();
 
         public Spreadsheet() {
 
@@ -21,14 +33,19 @@ namespace SS {
 
         public override object GetCellContents(string name) {
             if (IsValid(name)) {
-                return Cells[name].Contents;
+                if (IsNonEmptyCell(name)) {
+                    return NonEmptyCells[name].Contents;
+                }
+                else {
+                    return "";
+                }
             } else {
                 throw new InvalidNameException();
             }
         }
 
         public override IEnumerable<string> GetNamesOfAllNonemptyCells() {
-            return Cells.Keys;
+            return NonEmptyCells.Keys;
         }
 
         public override IList<string> SetCellContents(string name, double number) {
@@ -36,7 +53,7 @@ namespace SS {
                 throw new InvalidNameException();
             }
             else {
-                Cells.Add(name, new Cell(name, number));
+                NonEmptyCells.Add(name, new Cell(name, number));
                 return GetCellsToRecalculate(name).ToList();
             }
         }
@@ -46,7 +63,7 @@ namespace SS {
                 throw new InvalidNameException();
             }
             else {
-                Cells.Add(name, new Cell(name, text));
+                NonEmptyCells.Add(name, new Cell(name, text));
                 return GetCellsToRecalculate(name).ToList();
             }
         }
@@ -56,7 +73,7 @@ namespace SS {
                 throw new InvalidNameException();
             }
             else {
-                Cells.Add(name, new Cell(name, formula));
+                NonEmptyCells.Add(name, new Cell(name, formula));
 
                 foreach (string variable in formula.GetVariables()) {
                     DG.AddDependency(variable, name);
@@ -69,43 +86,70 @@ namespace SS {
             return DG.GetDependents(name);
         }
 
-        private bool IsValid(string name) {
-            string varPattern = @"[a-zA-Z_](?: [a-zA-Z_]|\d)*";
+        /// <summary>
+        /// Returns true if a string is a valid Cell name
+        /// Otherwise returns false
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        private static bool IsValid(string name) {
+            string varPattern = "^[_a-zA-Z][_a-zA-Z0-9]*";
             if (Regex.IsMatch(name, varPattern)) {
                 return true;
             }
             else return false;
         }
+
+        /// <summary>
+        /// Returns true if the name represents a non-empty cell
+        /// in the spreadsheet
+        /// Otherwise returns false
+        /// </summary>
+        /// <param name="name"> The cell name </param>
+        /// <returns> A boolean representing if the cell is non-empty</returns>
+        private bool IsNonEmptyCell(string name) {
+            return NonEmptyCells.ContainsKey(name);
+        }
     }
 
+    /// <summary>
+    /// A Cell represents a non-empty cell in a Spreadsheet
+    /// Cells have Names and Contents.
+    /// A string is a valid cell name if and only if:
+    ///   (1) its first character is an underscore or a letter
+    ///   (2) its remaining characters (if any) are underscores and/or letters and/or digits
+    ///
+    /// Contents can be a number, text or a Formula 
+    /// </summary>
     internal class Cell {
 
-        public string Name { get; set; }
-        public object Contents { get; set; }
+        private string cellName;
+        private object cellContents;
+
+        public object Contents {
+            get {
+                return cellContents;
+            }
+        }
+        public string Name {
+            get {
+                return cellName;
+            }
+        }
 
         public Cell(string name, double number) {
-            Name = name;
-            Contents = number;
-
+            cellName = name;
+            cellContents = number;
         }
 
         public Cell(string name, string text) {
-            Name = name;
-            Contents = text;
+            cellName = name;
+            cellContents = text;
         }
 
         public Cell(string name, Formula formula) {
-            Name = name;
-            Contents = formula;
-        }
-
-        public bool IsEmpty() {
-            if (Contents.ToString() == "") {
-                return true;
-            }
-            else {
-                return false;
-            }
+            cellName = name;
+            cellContents = formula;
         }
     }
 }
